d90677d4d984ae3afed0dfe20d5ebc91fbabf185
Luis Bermejo Lara <166100283+LuisBermejoL@users.noreply.github.com>
2025-12-05 13:27:48 +0100

    Add files via upload

---
+++ UT3-Programación Comunicaciones en Red/Ejercicios/TCPSocketCliente11.java
@@ -0,0 +1,41 @@
+package Ejercicios;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class TCPSocketCliente11 {
+
+    private static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) throws Exception {
+		InetAddress ipCliente = InetAddress.getByName("192.168.204.129");
+		int Puerto = 6666;//puerto remoto
+		
+		System.out.println("PROGRAMA CLIENTE 1 INICIADO....");
+		Socket Cliente = new Socket(ipCliente, Puerto);
+
+		// Creación flujo de salida hacia el servidor
+		DataOutputStream flujoSalida = new DataOutputStream(Cliente.getOutputStream());
+
+        System.out.println("Escribe un número: ");
+        int numero = sc.nextInt();
+		
+        System.out.println("Enviando el número " + numero + " al servidor desde el cliente 1");
+		flujoSalida.writeInt(numero);
+
+		// Creación flujo de entrada desde el servidor
+		DataInputStream flujoEntrada = new  DataInputStream(Cliente.getInputStream());
+
+		//Recibiendo datos del servidor
+        long resultado = flujoEntrada.readLong();
+		System.out.println("Recibiendo del servidor el resultado: " + resultado);
+
+		// CERRAR STREAMS Y SOCKETS	
+		flujoEntrada.close();
+		flujoSalida.close();
+		Cliente.close();
+	}// main
+}
